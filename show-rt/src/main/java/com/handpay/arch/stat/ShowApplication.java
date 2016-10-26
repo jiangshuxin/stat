1c0e82a3839d56d6d25b547119bdb77ae1e1145b
zhengfc <zhengfc323@gmail.com>
2016-10-26 16:10:46 +0800

    gogo

---
+++ show-rt/src/main/java/com/handpay/arch/stat/ShowApplication.java
@@ -0,0 +1,19 @@
+package com.handpay.arch.stat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Created by fczheng on 2016/10/26.
+ */
+
+@SpringBootApplication
+@ImportResource(locations = {"classpath:spring/spring-context", "classpath:spring/spring-web.xml"})
+public class ShowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShowApplication.class, args);
+    }
+
+}
--- show-rt/src/main/java/com/handpay/arch/stat/handler/StatResultHandler.java
+++ show-rt/src/main/java/com/handpay/arch/stat/handler/StatResultHandler.java
@@ -1,6 +1,9 @@
 package com.handpay.arch.stat.handler;
 
-import java.io.IOException;
+import com.alibaba.fastjson.JSON;
+import com.handpay.arch.common.ResultVO;
+import com.handpay.rache.core.spring.StringRedisTemplateX;
+import com.handpay.rache.core.spring.connection.StringRedisConnectionX;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
@@ -12,10 +15,7 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-import com.alibaba.fastjson.JSON;
-import com.handpay.arch.common.ResultVO;
-import com.handpay.rache.core.spring.StringRedisTemplateX;
-import com.handpay.rache.core.spring.connection.StringRedisConnectionX;
+import java.io.IOException;
 
 public class StatResultHandler extends TextWebSocketHandler {
 
